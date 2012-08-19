87ebf7122f7e9d1169e1f53fd7f18fba5d404873
Alberto Caso <alberto.caso@adaptia.es>
2012-08-19 16:11:10 +0200

    Factor out common prefix in lists module's urlpatterns

--- yampico/lists/urls.py
+++ yampico/lists/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'lists.views.index'),
-    url(r'^(?P<listid>\d+)/$', 'lists.views.detail'),
+urlpatterns = patterns('lists.views',
+    url(r'^$', 'index'),
+    url(r'^(?P<listid>\d+)/$', 'detail'),
     url(r'^add/$', 'lists.views.add'),
-    url(r'^(?P<listid>\d+)/edit/', 'lists.views.edit'),
-    url(r'^(?P<listid>\d+)/delete/', 'lists.views.delete'),
+    url(r'^(?P<listid>\d+)/edit/', 'edit'),
+    url(r'^(?P<listid>\d+)/delete/', 'delete'),
 
-    url(r'^(?P<listid>\d+)/update-items/$', 'lists.views.update_items'),
-    url(r'^(?P<listid>\d+)/add-item/$', 'lists.views.add_item'),
-    url(r'^\d+/delete-item/(?P<itemid>\d+)/$', 'lists.views.delete_item'),
+    url(r'^(?P<listid>\d+)/update-items/$', 'update_items'),
+    url(r'^(?P<listid>\d+)/add-item/$', 'add_item'),
+    url(r'^\d+/delete-item/(?P<itemid>\d+)/$', 'delete_item'),
 )
